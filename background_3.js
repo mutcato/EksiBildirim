1ec81bf6c07a143e758760ee1862988aabb1998e
mutcato <mtokat1@gmail.com>
2018-11-30 18:31:22 +0300

    google analytics to background.html

--- background_3.js
+++ background_3.js
@@ -309,6 +309,12 @@ function engine(){
 
 
 
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
 
 /*
 //Get email
