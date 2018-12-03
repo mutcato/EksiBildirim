aed3eacdb0cf683832279d2ef62afe7c9d71fd64
mcan543 <mtokat1@gmail.com>
2018-12-03 12:36:22 +0300

    gereksiz analytics kodu kaldırıldı

--- background_3.js
+++ background_3.js
@@ -309,13 +309,6 @@ function engine(){
 
 
 
-(function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = 'https://ssl.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-
 /*
 //Get email
 chrome.identity.getProfileUserInfo(function(userinfo){
